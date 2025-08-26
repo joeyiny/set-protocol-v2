f4f2a14d9be208091678fab165ea22b892e89f41
Joey Iny <joeyainy@gmail.com>
2025-08-26 17:12:21 -0400

    Add wagmi config

---
+++ wagmi.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "@wagmi/cli";
+import { foundry } from "@wagmi/cli/plugins";
+
+export default defineConfig({
+  out: "abis/CounterAbi.ts",
+  plugins: [
+    foundry({
+      project: "./src/",
+      include: ["protocol/**/*.sol"],
+    }),
+  ],
+});
