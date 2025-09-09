5edcf3f15c8433f2b022e2d0076f33513e70d4c9
Joey Iny <joeyainy@gmail.com>
2025-09-09 09:56:57 -0400

    Update wagmi.config.ts

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@wagmi/cli";
 import { foundry } from "@wagmi/cli/plugins";
 
 export default defineConfig({
-  out: "abis/generated.ts",
+  out: "../abi/src/set-protocol-v2-abis.ts",
   plugins: [
     foundry({
       project: "./",
